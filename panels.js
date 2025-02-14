7c015e0ce87b10adbb51e32c504becf49551daf9
Parker Ackerknecht <97491894+Parkerack@users.noreply.github.com>
2025-02-14 14:11:27 -0700

    Add files via upload
    Adjustment of .js to pull up the .jsx of the build-editor, only front end

---
+++ panels.js
@@ -0,0 +1,66 @@
+import SBOLEditorPanel from "./components/panels/sbol-editor/SBOLEditorPanel";
+import SimulatorPanel from "./components/panels/simulator/SimulatorPanel";
+import AssemblyPanel from "./components/panels/assembly-editor/AssemblyPanel";
+import BuildPanel from "./components/panels/build-editor/BuildPanel";
+import { CanvasIcon, SimulationIcon } from "./icons";
+import { ObjectTypes } from "./objectTypes";
+
+
+export const PanelTypes = {
+    Simulator: {
+        id: "synbio.panel-type.simulator",
+        title: "iBioSim Analysis",
+        component: SimulatorPanel,
+        objectTypes: [ ObjectTypes.Analysis.id ],
+        icon: SimulationIcon,
+
+        deserialize: content => {
+            try {
+                return JSON.parse(content)
+            }
+            catch {
+                return {}
+            }
+        },
+
+        serialize: panel => {
+            const { id, fileHandle, type, ...restOfPanel } = panel
+            return JSON.stringify(restOfPanel)
+        }
+    },
+    SBOLEditor: {
+        id: "synbio.panel-type.sbol-editor",
+        title: "SBOL Canvas",
+        component: SBOLEditorPanel,
+        objectTypes: [ ObjectTypes.SBOL.id ],
+        icon: CanvasIcon,
+
+        deserialize: content => ({
+            sbol: content
+        }),
+
+        serialize: panel => panel.sbol
+    },
+    AssemblyPlanCreator: {
+        id: "synbio.panel-type.assembly-plan-creator",
+        title: "Assembly Plan Creator",
+        component: AssemblyPanel,
+        objectTypes: [ ObjectTypes.Assembly.id ],
+        icon: SimulationIcon,
+    },
+    BuildPlanCreator: {
+        id: "synbio.panel-type.build-plan-creator",
+        title: "Build Plan Creator",
+        component: BuildPanel,
+        objectTypes: [ ObjectTypes.Build.id ],
+        icon: SimulationIcon,
+    }
+}
+
+export function getPanelType(id) {
+    return Object.values(PanelTypes).find(pt => pt.id == id)
+}
+
+export function getPanelTypeForObject(objectType) {
+    return Object.values(PanelTypes).find(pt => pt.objectTypes.includes(objectType))
+}
